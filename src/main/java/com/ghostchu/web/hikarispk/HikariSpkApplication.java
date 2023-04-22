b905097be8a95d5c091ab7af1cbf037323ea84c5
Ghost_chu <ghostchu@111.com>
2023-04-22 12:44:15 +0800

    close the stream

--- src/main/java/com/ghostchu/web/hikarispk/HikariSpkApplication.java
+++ src/main/java/com/ghostchu/web/hikarispk/HikariSpkApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 
 @SpringBootApplication
@@ -13,8 +14,8 @@ public class HikariSpkApplication {
     public static void main(String[] args) {
         File file = new File("application.yml");
         if (!file.exists()) {
-            try {
-                Files.copy(HikariSpkApplication.class.getResourceAsStream("/application.yml"), file.toPath());
+            try (InputStream stream = HikariSpkApplication.class.getResourceAsStream("/application.yml")) {
+                Files.copy(stream, file.toPath());
             } catch (IOException e) {
                 System.err.println("Cannot copy application.yml from jar to " + file.getAbsolutePath());
             }
